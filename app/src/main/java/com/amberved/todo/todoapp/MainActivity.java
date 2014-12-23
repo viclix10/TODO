9f6d4666161ae268b6fb54ae9028a88da2c533b5
amberved <amberved@gmail.com>
2014-12-23 08:48:08 -0800

    Add support for SQLite and DueDate for Todo tasks

--- app/src/main/java/com/amberved/todo/todoapp/MainActivity.java
+++ app/src/main/java/com/amberved/todo/todoapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.amberved.todo.todoapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,14 +8,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -24,21 +18,26 @@ public class MainActivity extends ActionBarActivity {
 
     private final int REQUEST_CODE = 100;
 
-    ArrayList<String> items;
-    ArrayAdapter<String> itemsAdapter;
+    ArrayList<Todo> items;
+    ArrayAdapter<Todo> itemsAdapter;
     ListView lvItems;
+
+    // DAO
+    private TodoDAO dao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         lvItems = (ListView) findViewById(R.id.lvItems);
 
-        //Read Previous Setting from file
+        // Create DAO object
+        dao = new TodoDAO(this);
+        //Read Previous Settings
         readItems();
 
-        itemsAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, items);
+        itemsAdapter = new ArrayAdapter<Todo>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
 
         //Is this the correct way to make list scroll to bottom
@@ -54,9 +53,10 @@ public class MainActivity extends ActionBarActivity {
     private void setupListViewLongClickListener() {
         lvItems.setOnItemLongClickListener( new AdapterView.OnItemLongClickListener() {
             public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                Todo todo = items.get(pos);
+                dao.deleteTodo(todo.getId());
                 items.remove(pos);
                 itemsAdapter.notifyDataSetChanged();
-                writeItems();
                 return true;
             }
         });
@@ -66,10 +66,14 @@ public class MainActivity extends ActionBarActivity {
     private void setupListViewClickListener() {
         lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> adapter, View item, int pos, long id) {
-                Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                Intent i = new Intent(MainActivity.this, AddEditItemActivity.class);
 
-                i.putExtra("todoText", items.get(pos).toString());
-                i.putExtra("pos", pos);
+                Todo todo = new Todo(items.get(pos).getText(), items.get(pos).getDueDate());
+
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+
+                i.putExtra("todo", todo);
 
                 startActivityForResult(i, REQUEST_CODE);
             }
@@ -79,15 +83,19 @@ public class MainActivity extends ActionBarActivity {
     // Get the edited todo and store it
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
-            String todoText = data.getExtras().getString("todoText").trim();
-            int pos = data.getIntExtra("pos", 0);
+
+            Todo todo = (Todo) data.getSerializableExtra("todo");
+
+            String todoText = todo.getText().trim();
+
             if (!todoText.isEmpty()) {
-                items.set(pos, todoText);
+                items.add(todo);
                 itemsAdapter.notifyDataSetChanged();
-                writeItems();
+                dao.createTodo(todo);
+                //DebugOnly
+                Toast.makeText(this, "deb"+todoText, Toast.LENGTH_SHORT).show();
             }
-            //DebugOnly
-            //Toast.makeText(this, todoText, Toast.LENGTH_SHORT).show();
+
         }
     }
 
@@ -100,49 +108,35 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.main_menu_settings) {
             return true;
-        }
+        } else if (id == R.id.main_menu_add) {
+            // Create an intent
+            Todo todo = new Todo();
 
+            Intent i = new Intent(this, AddEditItemActivity.class);
+            i.putExtra("todo", todo);
+
+            // Start activity
+            startActivityForResult(i, REQUEST_CODE);
+
+            return true;
+        } else if (id == R.id.main_menu_exit) {
+
+            //Close the database
+            dao.close();
+
+            // Finish this activity
+            this.finish();
+        }
         return super.onOptionsItemSelected(item);
     }
 
-    //Add a new Item to the list on button pressed
-    public void onAddItem(View v) {
-        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
-        String itemText = etNewItem.getText().toString().trim();
-        if (!itemText.isEmpty()) {
-            itemsAdapter.add(itemText);
-            etNewItem.setText("");
-            writeItems();
-        }
-    }
-
     private void readItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File (filesDir, "todo.txt");
-        try {
-            items = new ArrayList<String>(FileUtils.readLines(todoFile));
-        } catch (IOException e)
-        {
-            items = new ArrayList<String>();
-        }
+        items = (ArrayList<Todo>) dao.getTodos();
     }
 
-    private void writeItems() {
-        File filesDir = getFilesDir();
-        File todoFile = new File (filesDir, "todo.txt");
-        try {
-            FileUtils.writeLines(todoFile, items);
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
 }
